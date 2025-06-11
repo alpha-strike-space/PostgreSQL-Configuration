6eaf87ae37f5175388439cf631bbd8863caa7e9b
kanderson <81449406+kandrsn99@users.noreply.github.com>
2025-06-11 13:25:06 -0500

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -54,7 +54,7 @@ $$ LANGUAGE plpgsql;
       Precomputed index to speed up requests related to time filtration, UNIX format. Prior iterations had LDAP.
 */
 CREATE INDEX idx_incident_converted_ts
-ON incident (((time_stamp - 116444736000000000) / 10000000.0));
+ON incident (to_timestamp((time_stamp - 116444736000000000) / 10000000.0));
 /*
       Precomputed index to speed up requests regarding name search for totals.
 */
